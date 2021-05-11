3ecc0bc3f92d71c0fe84659b3f35479263e1e5c3
kbuczko <mr.acer1998@gmail.com>
2021-05-11 09:31:17 +0200

    Update home.Designer.cs

--- Biblioteka/home.Designer.cs
+++ Biblioteka/home.Designer.cs
@@ -40,9 +40,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(308, 12);
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(378, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(166, 41);
+            this.button1.Size = new System.Drawing.Size(194, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "Fsl";
             this.button1.UseVisualStyleBackColor = true;
@@ -50,9 +52,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(308, 82);
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(378, 101);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(166, 41);
+            this.button2.Size = new System.Drawing.Size(194, 50);
             this.button2.TabIndex = 1;
             this.button2.Text = "Materialy";
             this.button2.UseVisualStyleBackColor = true;
@@ -60,9 +64,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(308, 147);
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(378, 181);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(166, 41);
+            this.button3.Size = new System.Drawing.Size(194, 50);
             this.button3.TabIndex = 2;
             this.button3.Text = "Parametry anten";
             this.button3.UseVisualStyleBackColor = true;
@@ -70,9 +76,12 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(308, 208);
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.Location = new System.Drawing.Point(378, 256);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(166, 41);
+            this.button4.Size = new System.Drawing.Size(194, 50);
             this.button4.TabIndex = 3;
             this.button4.Text = "Kable";
             this.button4.UseVisualStyleBackColor = true;
@@ -80,9 +89,11 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(308, 269);
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(378, 331);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(166, 41);
+            this.button5.Size = new System.Drawing.Size(194, 50);
             this.button5.TabIndex = 4;
             this.button5.Text = "Zlacza";
             this.button5.UseVisualStyleBackColor = true;
@@ -90,9 +101,11 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(308, 331);
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button6.Location = new System.Drawing.Point(378, 407);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(166, 41);
+            this.button6.Size = new System.Drawing.Size(194, 50);
             this.button6.TabIndex = 5;
             this.button6.Text = "Czestotliwosc";
             this.button6.UseVisualStyleBackColor = true;
@@ -100,9 +113,11 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(308, 397);
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button7.Location = new System.Drawing.Point(378, 489);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(166, 41);
+            this.button7.Size = new System.Drawing.Size(194, 50);
             this.button7.TabIndex = 6;
             this.button7.Text = "Odleglosc";
             this.button7.UseVisualStyleBackColor = true;
@@ -110,9 +125,11 @@
             // 
             // QuitButton
             // 
-            this.QuitButton.Location = new System.Drawing.Point(713, 415);
+            this.QuitButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.QuitButton.Location = new System.Drawing.Point(832, 505);
+            this.QuitButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.QuitButton.Name = "QuitButton";
-            this.QuitButton.Size = new System.Drawing.Size(75, 23);
+            this.QuitButton.Size = new System.Drawing.Size(87, 34);
             this.QuitButton.TabIndex = 7;
             this.QuitButton.Text = "Wyjdź";
             this.QuitButton.UseVisualStyleBackColor = true;
@@ -120,9 +137,9 @@
             // 
             // home
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 554);
             this.Controls.Add(this.QuitButton);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
@@ -131,6 +148,8 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "home";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "home";
